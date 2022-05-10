956060889b1c8e229f94a40cfd0f5626d8f6c582
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2022-05-10 17:49:19 +0200

    refactor: remove redundant String conversion

--- src/Customer.java
+++ src/Customer.java
@@ -35,12 +35,12 @@ class Customer {
       }
       //show figures for this rental
       result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" +
-          String.valueOf(thisAmount) + "\n";
+          thisAmount + "\n";
       totalAmount += thisAmount;
     }
     //add footer lines
-    result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-    result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+    result += "Amount owed is " + totalAmount + "\n";
+    result += "You earned " + frequentRenterPoints + " frequent renter points";
     return result;
   }
 
