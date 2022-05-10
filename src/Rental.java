c2d514dc589db0f4e39b825f521c14e1f957f58c
ncryptedV1 <15632467+ncryptedV1@users.noreply.github.com>
2022-05-10 18:12:33 +0200

    refactor: inlined if for rental amount calculation

--- src/Rental.java
+++ src/Rental.java
@@ -19,19 +19,13 @@ class Rental {
     double thisAmount = 0;
     switch (movie.getPriceCode()) {
       case REGULAR:
-        thisAmount += 2;
-        if (daysRented > 2) {
-          thisAmount += (daysRented - 2) * 1.5;
-        }
+        thisAmount += 2 + Math.max(daysRented - 2, 0) * 1.5;
         break;
       case NEW_RELEASE:
         thisAmount += daysRented * 3;
         break;
       case CHILDREN:
-        thisAmount += 1.5;
-        if (daysRented > 3) {
-          thisAmount += (daysRented - 3) * 1.5;
-        }
+        thisAmount += 1.5 + Math.max(daysRented - 3, 0) * 1.5;
         break;
     }
     return thisAmount;
